9b9021b2791a50ea405135f6c91b4178345e3e5b
Hawer Torres <Hawer.X.Torres.-ND@Disney.com>
2016-09-16 14:19:00 -0500

    Implement  methods to load team with players

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -2,4 +2,57 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Player Collection
+
+var players = [["Name" :"Hawer", "": 3],
+    ["Name" :"Cristiano"],
+    ["Name" :"Messi"],
+    ["Name" :"Maradona"],
+    ["Name" :"Pele"],
+    ["Name" :"Zidane"],
+    ["Name" :"Ronaldiño"]
+]
+
+// Team collection
+
+var teams = [
+    "Dragons": [[:]],
+    "Sharks": [[:]],
+    "Raptors": [[:]]
+]
+
+var maxPlayersByTeam = (players.count) / (teams.count)
+
+func loadPlayersInTeam(teamName: String)
+{
+    // Create a temporal array to store player by team
+    var team = [[String:AnyObject]]()
+    var count = 0
+    for i in 0..<maxPlayersByTeam {
+        let player = players[i]
+        team.append(player)
+        count++
+        players.removeAtIndex(i)
+        
+        // if team is loaded assign it to team collection
+        if (count == maxPlayersByTeam)
+        {
+            teams[teamName] = team
+            count = 0;
+        }
+    }
+}
+
+func configureTeams()
+{
+    for (key, _) in teams {
+        loadPlayersInTeam(key)
+    }
+
+}
+
+configureTeams()
+
+print(teams["Dragons"])
+print(teams["Sharks"])
+print(teams["Raptors"])
